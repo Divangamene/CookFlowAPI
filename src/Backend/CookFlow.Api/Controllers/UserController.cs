52a6b8ffaa53188a60eec297dad59a35311440f1
Divangamene <divangameneluyeye@gmail.com>
2026-05-12 01:59:15 +0100

    feat: Adicionar Endpoint de Registro do usuario

---
+++ src/Backend/CookFlow.Api/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using CookFlow.Communication.Request;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookFlow.api.Controllers;
+
+[Route("api/[controller]")]
+public class UserController : Controller
+{
+
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestRegisterUserAccountJson request)
+    {
+        return Created();
+    }
+}
---
+++ src/Shared/CookFlow.Communication/Request/RequestRegisterUserAccountJson.cs
@@ -0,0 +1,8 @@
+﻿namespace CookFlow.Communication.Request;
+
+ public class RequestRegisterUserAccountJson
+{
+    public string Nome{ get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
