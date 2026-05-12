68a7c561745dab6d79ba2d7d4a6a289d4bcad7de
Divangamene <divangameneluyeye@gmail.com>
2026-05-12 05:49:52 +0100

    feat(swagger): testar endpoint de cadastro de usuário

--- src/Shared/CookFlow.Communication/Request/RequestRegisterUserAccountJson.cs
+++ src/Shared/CookFlow.Communication/Request/RequestRegisterUserAccountJson.cs
@@ -3,6 +3,7 @@
  public class RequestRegisterUserAccountJson
 {
 //meus requests
+//
     public string Nome { get; set; } = string.Empty;
     public string Email { get; set; }= string.Empty;
     public string Password { get; set; } = string.Empty;
