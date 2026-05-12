9c9a34dd40d429efcdb8b2d90bd572c1515a7f3b
Divangamene <divangameneluyeye@gmail.com>
2026-05-12 05:29:42 +0100

    fix(config): corrigir launchSettings do swagger

---
+++ src/Backend/CookFlow.Api/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CookFlow.api.Controllers;
+
+[Route("Api/[controller]")]
+public class UserController : Controller
+{
+    [HttpPost]
+    public IActionResult Register()
+    {
+        return Created();
+    }
+}
